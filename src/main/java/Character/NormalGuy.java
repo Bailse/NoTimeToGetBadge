4359c00e5add353198f68b79267a8f4b4d25f590
Chawiss Sangcharoen <6833047021@student.chula.ac.th>
2026-03-01 20:51:40 +0700

    Building v.3

--- src/main/java/Character/NormalGuy.java
+++ src/main/java/Character/NormalGuy.java
@@ -26,34 +26,4 @@ public class NormalGuy extends BasePlayer{
 
     }
 
-    @Override
-    public boolean work(int staminaCost, int moneyGain) {
-        // 1. เรียกใช้ Logic ของ BasePlayer แต่ส่งค่าที่ "ดีกว่า" เข้าไป
-        // เช่น ถ้ากดทำงาน 10 หน่วย NormalGuy จะเสียแค่ 9 และได้เงินเพิ่ม 100
-        boolean isSuccess = super.work(staminaCost - 1, moneyGain + 100);
-
-        // 2. ส่งคืนผลลัพธ์ที่ได้จากคลาสแม่กลับไปให้ Popup
-        // ถ้า super.work คืนค่า true (พลังพอ) ตรงนี้ก็จะส่ง true กลับไป
-        return isSuccess;
-    }
-
-    private int workCount = 0;
-
-    public String earnWorkBonus() { //Restuarant
-        this.workCount++;
-
-        if (this.workCount >= 5) {
-            int bonusMoney = 20000;
-            this.setMoney(this.getMoney() + bonusMoney);
-
-            this.workCount = 0; // รีเซ็ตแต้ม
-            return "OTAKU_BONUS_ACTIVATED"; // สถานะเมื่อได้โบนัสใหญ่
-        }
-
-        return "OTAKU_PROGRESS_" + this.workCount; // สถานะสะสมแต้มปกติ
-    }
-
-    public int getWorkCount() {
-        return workCount;
-    }
 }
